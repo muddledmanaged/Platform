adc275a298217bce259cfce322848de1789e0a1b
Wahid Tanner <wahidtanner@gmail.com>
2014-08-11 22:31:34 -0400

    Added extra test cases for empty string literals.

--- PlatformDesign/StringLiteralScenarios.cpp
+++ PlatformDesign/StringLiteralScenarios.cpp
@@ -76,3 +76,47 @@ DESIGNER_SCENARIO( StringLiteral, "Compilation/Normal", "StringLiteral can be co
     verifyEqual('I', combinedLiteral[8]);
     verifyEqual('\0', combinedLiteral[9]);
 }
+
+DESIGNER_SCENARIO( StringLiteral, "Compilation/Empty", "StringLiteral can be constructed with an empty string literal." )
+{
+    constexpr Platform::StringLiteral literal = "";
+
+    verifyEqual('\0', literal[0]);
+}
+
+DESIGNER_SCENARIO( StringLiteral, "Compilation/Empty", "StringLiteral can append an empty string literal." )
+{
+    constexpr Platform::StringLiteral literal = "ABC";
+    Platform::StringLiteral combinedLiteral = literal + "";
+
+    verifyEqual('A', combinedLiteral[0]);
+    verifyEqual('B', combinedLiteral[1]);
+    verifyEqual('C', combinedLiteral[2]);
+    verifyEqual('\0', combinedLiteral[3]);
+}
+
+DESIGNER_SCENARIO( StringLiteral, "Compilation/Empty", "StringLiteral can append multiple empty string literals." )
+{
+    constexpr Platform::StringLiteral literal = "ABC";
+    Platform::StringLiteral combinedLiteral = literal + "" + "";
+
+    verifyEqual('A', combinedLiteral[0]);
+    verifyEqual('B', combinedLiteral[1]);
+    verifyEqual('C', combinedLiteral[2]);
+    verifyEqual('\0', combinedLiteral[3]);
+}
+
+DESIGNER_SCENARIO( StringLiteral, "Compilation/Empty", "StringLiteral can be constructed with an embedded empty string literal." )
+{
+    constexpr Platform::StringLiteral literal1 = "ABC";
+    constexpr Platform::StringLiteral literal3 = "DEF";
+    Platform::StringLiteral combinedLiteral = literal1 + "" + literal3;
+
+    verifyEqual('A', combinedLiteral[0]);
+    verifyEqual('B', combinedLiteral[1]);
+    verifyEqual('C', combinedLiteral[2]);
+    verifyEqual('D', combinedLiteral[3]);
+    verifyEqual('E', combinedLiteral[4]);
+    verifyEqual('F', combinedLiteral[5]);
+    verifyEqual('\0', combinedLiteral[6]);
+}
